423bb2b98bb01fc1b15498c2c27c4fe52c4493c1
Dimitri <69167058+dimsome@users.noreply.github.com>
2022-05-03 16:35:04 +0700

    feat: add verify task

---
+++ tasks/verifyContract.ts
@@ -0,0 +1,27 @@
+import { task, types } from 'hardhat/config'
+// import { IERC20, IERC20__factory } from 'types/generated'
+
+import { verifyEtherscan } from '../utils/etherscan'
+
+task('verifyEtherscan', 'Verify the deployed contract on Etherscan')
+  .addParam(
+    'address',
+    'Address for the contract to verify.',
+    undefined,
+    types.string,
+    false,
+  )
+  .setAction(async (taskArgs, hre) => {
+    // Deployed address: 0x783CC67242fd639a7621eA1A1C511E4C64D7C66d
+    const address = taskArgs.address
+
+    const balToken = '0xba100000625a3754423978a60c9317c58a424e3D'
+
+    await verifyEtherscan(hre, {
+      address,
+      constructorArguments: [balToken],
+      contract: 'contracts/MerkleDrop.sol:MerkleDrop',
+    })
+
+    console.log('Done!')
+  })
---
+++ utils/etherscan.ts
@@ -0,0 +1,30 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+export const sleep = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms))
+
+interface VerifyEtherscan {
+  address: string
+  contract?: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+  constructorArguments?: any[]
+  libraries?: {
+    [libraryName: string]: string
+  }
+}
+
+export const verifyEtherscan = async (
+  hre: HardhatRuntimeEnvironment,
+  contract: VerifyEtherscan,
+): Promise<void> => {
+  // wait for the Etherscan backend to pick up the deployed contract
+  await sleep(10000)
+
+  console.log(`called verifyEtherscan with ${JSON.stringify(contract)}`)
+  console.log(`hre.network.name is ${hre.network.name}`)
+
+  if (hre.network.name !== 'hardhat') {
+    console.log(`About to verify ${contract.address} on Etherscan`)
+    await hre.run('verify:verify', contract)
+  }
+}
